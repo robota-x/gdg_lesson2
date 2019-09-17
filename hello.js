22bee8aef85776b5d83455466f0687cdad6ed4aa
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-09-17 22:30:22 +0100

    move to an array

--- hello.js
+++ hello.js
@@ -1,12 +1,6 @@
-const name_1 = 'lorenzo';
-const name_2 = 'second_name';
-const name_3 = 'third_name';
-const name_4 = 'fourth_name';
+const nameList = ["lorenzo", "second_name", "third_name", "fourth_name"];
 
-
-console.log(`hello ${name_1}`);
-console.log(`hello ${name_2}`);
-console.log(`hello ${name_3}`);
-console.log(`hello ${name_4}`);
-
-
+console.log(`hello ${nameList[0]}`);
+console.log(`hello ${nameList[1]}`);
+console.log(`hello ${nameList[2]}`);
+console.log(`hello ${nameList[3]}`);

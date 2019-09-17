d0d414f7d68cd6292cda572c16a17c11dddf4562
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-09-17 22:31:36 +0100

    move to an array

--- hello.js
+++ hello.js
@@ -1,6 +1,10 @@
 const nameList = ["lorenzo", "second_name", "third_name", "fourth_name"];
 
-console.log(`hello ${nameList[0]}`);
-console.log(`hello ${nameList[1]}`);
-console.log(`hello ${nameList[2]}`);
-console.log(`hello ${nameList[3]}`);
+
+const sayHi = name => console.log(`hello ${name}`);
+
+
+sayHi(nameList[0]);
+sayHi(nameList[1]);
+sayHi(nameList[2]);
+sayHi(nameList[3]);

724e017208cb12c030b5df84d9a4713af8ded77b
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-09-17 22:43:43 +0100

    import chalk in main file

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
 const sayHi = name => console.log(`hello ${name}`);
 
-const sayGoodbye = name => console.log(`it is now ${new Date()} and time to say goodbye, ${name}`)
+const sayGoodbye = name => console.log(`it is now ${new Date()} and time to say goodbye, ${chalk.blue(name)}`)
 
 exports.sayHi = sayHi;
 exports.sayGoodbye = sayGoodbye;

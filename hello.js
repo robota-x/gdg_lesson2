b9e6db6c5226076627c809d4c1785174e4e97efe
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-09-17 22:32:13 +0100

    use forEach array method and callback

--- hello.js
+++ hello.js
@@ -3,8 +3,4 @@ const nameList = ["lorenzo", "second_name", "third_name", "fourth_name"];
 
 const sayHi = name => console.log(`hello ${name}`);
 
-
-sayHi(nameList[0]);
-sayHi(nameList[1]);
-sayHi(nameList[2]);
-sayHi(nameList[3]);
+nameList.forEach(sayHi);

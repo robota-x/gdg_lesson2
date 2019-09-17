c00bfafee201abc662cfd26944ea2ce40780582d
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-09-17 22:38:55 +0100

    add goodbye function with double interpolation

--- helpers.js
+++ helpers.js
@@ -1,3 +1,6 @@
 const sayHi = name => console.log(`hello ${name}`);
 
+const sayGoodbye = name => console.log(`it is now ${new Date()} and time to say goodbye, ${name}`)
+
 exports.sayHi = sayHi;
+exports.sayGoodbye = sayGoodbye;
--- main.js
+++ main.js
@@ -3,3 +3,5 @@ var helpers = require('./helpers.js')
 const nameList = ["lorenzo", "second_name", "third_name", "fourth_name"];
 
 nameList.forEach(helpers.sayHi);
+
+nameList.forEach(helpers.sayGoodbye);

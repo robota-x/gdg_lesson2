2f1424461d020a657e43b1b5f5052a5e44060fc7
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-09-17 22:29:11 +0100

    say hello to 4 people

--- hello.js
+++ hello.js
@@ -1,4 +1,12 @@
-const name = 'lorenzo';
+const name_1 = 'lorenzo';
+const name_2 = 'second_name';
+const name_3 = 'third_name';
+const name_4 = 'fourth_name';
 
 
-console.log(`hello ${name}`);
+console.log(`hello ${name_1}`);
+console.log(`hello ${name_2}`);
+console.log(`hello ${name_3}`);
+console.log(`hello ${name_4}`);
+
+
